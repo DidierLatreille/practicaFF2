e5c0ba26f034ae99d36cb6827b0c3f0395cf77fe
DidierLatreille <didier.latreille@gmail.com>
2021-02-19 17:43:11 -0300

    primerPush

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,38 @@
 module Lib where
-import Text.Show.Functions
 
 laVerdad = True
+
+type ColorCorbata = String
+type NombreMozo = String
+type CriterioPropina = Double -> Double
+
+propinaRecomendada :: CriterioPropina
+propinaRecomendada costoComida = costoComida * 0.1
+
+propinaMrPink :: CriterioPropina
+propinaMrPink _ = 0
+
+propinaConservadora :: CriterioPropina
+propinaConservadora costoComida
+  | (even . floor) costoComida = 20
+  | otherwise                  = 20 + 5
+
+propinaTioCarlos :: NombreMozo -> CriterioPropina
+propinaTioCarlos "carlos"  costoComida = ((* 2) . propinaRecomendada) costoComida
+propinaTioCarlos nombreMozo costoComida = ((/ 2) . propinaRecomendada) costoComida
+
+costoTotal :: CriterioPropina -> Double -> Double
+costoTotal propina costoComida = costoComida + propina costoComida
+
+propinaPayasos :: ColorCorbata -> CriterioPropina
+propinaPayasos "azul" costoComida = propinaRecomendada costoComida
+propinaPayasos "rojo" _ = 100
+propinaPayasos "negro" _ = 0
+
+propinaSegunCosto :: CriterioPropina
+propinaSegunCosto costoComida | costoComida <= 500 = ((3*).propinaRecomendada) costoComida 
+                              | costoComida < 1000 = propinaConservadora costoComida
+                              | otherwise = 0
+
+mozoSatisfecho :: CriterioPropina -> Double -> Bool
+mozoSatisfecho propina costoComida = propina costoComida >= costoComida * 0.15
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,62 @@ import Lib
 
 main :: IO()
 main = hspec $ do
-   describe "Tests de prueba" $ do
-      it "la verdad y la verdad" $ do
-         laVerdad `shouldBe` True
+   describe "costoTotal" $ do
+      it "costoTotal con 'costoComida 500' y propinaRecomendada = 550" $ do
+         costoTotal propinaRecomendada 500 `shouldBe` 550
+
+      it "costoTotal con 'costoComida 500' y propinaMrPink = 500" $ do
+         costoTotal propinaMrPink 500 `shouldBe` 500
+
+      it "costoTotal con 'costoComida 500' y propinaConservadora = 520" $ do
+         costoTotal propinaConservadora 500 `shouldBe` 520
+
+      it "costoTotal con 'costoComida 501' y propinaConservadora = 526" $ do
+         costoTotal propinaConservadora 501 `shouldBe` 526
+
+      it "costoTotal con 'costoComida 500', propinaTioCarlos y el mozo se llama carlos= 600" $ do
+         costoTotal (propinaTioCarlos "carlos") 500 `shouldBe` 600
+
+      it "costoTotal con 'costoComida 500', propinaTioCarlos y el mozo se llama Jojo = 525" $ do
+         costoTotal (propinaTioCarlos "Jojo") 500 `shouldBe` 525
+
+      it "costoTotal con 'costoComida 500' y propinaPayasos = 550" $ do
+         costoTotal (propinaPayasos "azul") 500 `shouldBe` 550
+
+      it "costoTotal con 'costoComida 500' y propinaPayasos = 600" $ do
+         costoTotal (propinaPayasos "rojo") 500 `shouldBe` 600
+
+      it "costoTotal con 'costoComida 500' y propinaPayasos = 500" $ do
+         costoTotal (propinaPayasos "negro") 500 `shouldBe` 500
+
+      it "costoTotal con 'costoComida 400' y propinaSegunCosto = 520" $ do
+         costoTotal propinaSegunCosto 400 `shouldBe` 520
+
+      it "costoTotal con 'costoComida 600' y propinaSegunCosto = 620" $ do
+         costoTotal propinaSegunCosto 600 `shouldBe` 620
+      
+      it "costoTotal con 'costoComida 601' y propinaSegunCosto = 626" $ do
+         costoTotal propinaSegunCosto 601 `shouldBe` 626
+      
+      it "costoTotal con 'costoComida 1200' y propinaSegunCosto = 1200" $ do
+         costoTotal propinaSegunCosto 1200 `shouldBe` 1200
+      
+      -- ¿Qué concepto aparece en la consulta del punto b. que no aparece en el punto a?
+      -- NOTA: las consultas del punto a y b son las dos que se encuentran a continuacion
+      -- en la consulta del punto b (la del tio carlos) aparece el concepto de pattern matching
+
+      it "costoTotal con 'costoComida 100' y propinaRecomendada = 110" $ do
+         costoTotal propinaRecomendada 100 `shouldBe` 110
+
+      it "costoTotal con 'costoComida 200', propinaTioCarlos y el mozo se llama Juan = 210 " $ do
+         costoTotal (propinaTioCarlos "juan") 200 `shouldBe` 210
+   
+   describe "mozoSatisfecho" $ do
+
+      it "Mozo satisfecho con propinaSegunCosto y 'costoComida 400'" $ do
+         mozoSatisfecho propinaSegunCosto 400 `shouldBe` True
+
+      it "Mozo NO satisfecho con propinaSegunCosto y 'costoComida 900'" $ do
+         mozoSatisfecho propinaSegunCosto 900 `shouldBe` False
+
+
